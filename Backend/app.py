6508def8b4b79bdf1e54f142746ad2bf31ad7282
sherin stanley <sherinS216@gmail.com>
2026-01-30 14:20:01 -0400

    Update app.py, add requirements.txt and log file

--- Backend/app.py
+++ Backend/app.py
@@ -81,6 +81,7 @@ def index():
 
     return render_template("index.html", prediction=prediction)
 
-
 if __name__ == "__main__":
-    app.run()
+    print("Starting Flask server at http://127.0.0.1:5000")
+    app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
+
